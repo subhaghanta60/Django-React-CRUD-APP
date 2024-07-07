869f81d311ac64fbb761385af4b877c90c48a0e1
Subha Ghanta <Subhaghanta60@gmail.com>
2024-07-07 10:30:07 +0530

    Set up React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
-
 import './App.css';
-
+import {Routes,Route} from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Create from './components/Create';
 function App() {
   return (
     <div className="App">
-      <div>Our Application</div>
+      <Routes>
+          <Route path="" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/create" element={<Create/>}/>
+      </Routes>
     </div>
   );
 }
