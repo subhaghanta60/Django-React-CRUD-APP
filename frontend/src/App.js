7d83788dfd15e2c2e16fd458039ced0c55b01b95
Subha Ghanta <Subhaghanta60@gmail.com>
2024-07-12 23:52:23 +0530

    Edit Funcanality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import About from './components/About';
 import Create from './components/Create';
 import Navbar from './components/Navbar';
+import Edit from './components/Edit';
 function App() {
   const myWidth=200;
   return (
@@ -14,6 +15,7 @@ function App() {
                     <Route path="" element={<Home/>}/>
                     <Route path="/about" element={<About/>}/>
                     <Route path="/create" element={<Create/>}/>
+                    <Route path="/edit/:id" element={<Edit/>}/>
                 </Routes>
 
               }
